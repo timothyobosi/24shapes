0fca0023801cba4c793c0bd8408e2233667bc31e
timothyobosi <timothyobosi8@gmail.com>
2025-11-21 11:19:14 +0300

    error fix

--- src/sections/ErrorPage.tsx
+++ src/sections/ErrorPage.tsx
@@ -4,7 +4,8 @@ function ErrorPage() {
   return (
     <div className={styles.errorContainer}>
       <div className={styles.errorContent}>
-        <h1 className={styles.errorTitle}>Error</h1>
+        <h1 className={styles.errorTitle}>404</h1>
+        <h2 className={styles.errorTitle}>Error</h2>
         <p className={styles.errorSubtitle}>Kindly contact IT support</p>
       </div>
     </div>
